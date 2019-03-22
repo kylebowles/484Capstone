4bec7ebf5322356c025896b68f2f7fcb5af76538
Nolan <48460388+kellynp1@users.noreply.github.com>
2019-03-22 16:45:03 -0400

    Add files via upload

---
+++ JobSearchPage.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class JobSearchPage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+
+
+        String searchTerm;
+
+        searchTerm = txtSearch.Value;
+
+        Session["temp"] = searchTerm;
+        searchGV.Visible = true;
+        searchGV.DataBind();
+
+
+    }
+}
