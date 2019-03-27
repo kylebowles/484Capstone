8b090c4398535a1d1563809c87ba9792bdfb652c
kylebowles <k.t.bowles12@gmail.com>
2019-03-27 14:11:23 -0400

    Update and rename CreateScriptKyle-TestNextTimeForUserLogin.sql to CreateScript.sql

--- CreateScript.sql
+++ CreateScript.sql
@@ -1,5 +1,3 @@
-USE [master]
-GO
 /****** Object:  Database [Cued-In]    Script Date: 3/22/2019 5:00:33 PM ******/
 CREATE DATABASE [Cued-In]
  CONTAINMENT = NONE
@@ -77,7 +75,8 @@ EXEC sys.sp_db_vardecimal_storage_format N'Cued-In', N'ON'
 GO
 ALTER DATABASE [Cued-In] SET QUERY_STORE = OFF
 GO
-
+USE [Cued-In]
+GO
 ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = OFF;
 GO
 ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET LEGACY_CARDINALITY_ESTIMATION = PRIMARY;
@@ -213,8 +212,6 @@ CREATE TABLE [dbo].[Employer](
 	[PersonID] [int] NOT NULL,
 	[isApproved] [nvarchar](5) NOT NULL,
 	[EmployerName] [nvarchar](100) NOT NULL,
-	[JobTitle] [nvarchar](50),
-	[EmployerSummary] [nvarchar](1000),
  CONSTRAINT [PK_Employer] PRIMARY KEY CLUSTERED 
 (
 	[EmployerID] ASC
@@ -298,7 +295,6 @@ CREATE TABLE [dbo].[Person](
 	[MiddleName] [nvarchar](50) NULL,
 	[LastName] [nvarchar](50) NOT NULL,
 	[Email] [nvarchar](50) NOT NULL,
-	[PhoneNumber] [nvarchar](12) NOT NULL,
 	[AddressID] [int] NOT NULL,
 	[personType] [nvarchar](10) NOT NULL,
  CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED 
@@ -424,25 +420,6 @@ CREATE TABLE [dbo].[StudentOpportunity](
 	[OpportunityID] [int] NOT NULL
 ) ON [PRIMARY]
 GO
-CREATE TABLE [dbo].[User_LogTime](
-	[LogID] [int] IDENTITY(1,1) NOT NULL,
-	[PersonID] [int] NULL,
-	[SID] [nvarchar](50) NULL,
-	[Login_Time] [datetime] NULL,
-	[Logout_Time] [datetime] NULL,
-	[offline] [bit] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[LogID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[User_LogTime]  WITH CHECK ADD FOREIGN KEY([PersonID])
-REFERENCES [dbo].[Person] ([PersonID])
-GO
-
-
 ALTER TABLE [dbo].[Account]  WITH CHECK ADD  CONSTRAINT [FK_Account_Person] FOREIGN KEY([PersonID])
 REFERENCES [dbo].[Person] ([PersonID])
 GO
@@ -613,29 +590,23 @@ REFERENCES [dbo].[Student] ([StudentID])
 GO
 ALTER TABLE [dbo].[StudentOpportunity] CHECK CONSTRAINT [FK_StudentOpportunity_Student]
 GO
-USE [Cued-in]
+USE [Cued-In]
 GO
-ALTER DATABASE [Cued-In] SET  READ_WRITE 
-GO
-
- ALTER TABLE [dbo].[Request]
+ALTER TABLE [dbo].[Request]
 DROP CONSTRAINT FK_Request_Application;
-GO
+
 ALTER TABLE [dbo].[Application]
 DROP CONSTRAINT FK_Application_Request;
-GO
+
 
 ALTER TABLE [dbo].[Application]
 DROP CONSTRAINT FK_Application_Opportuntiy;
-GO
+
 ALTER TABLE [dbo].[Attachment]
-ALTER COLUMN Content varchar(100);	       
-USE [master]
-GO
+ALTER COLUMN Content varchar(100);
 ALTER DATABASE [Cued-In] SET  READ_WRITE 
 GO
-Use [Cued-In]
-GO
+
 insert into address ([HouseNumber],[Street],[City],[State],[Country],[Zipcode]) values (1325, 'Devon Ln', 'Harrisonburg', 'VA','United States', 22801);
 insert into address ([HouseNumber],[Street],[City],[State],[Country],[Zipcode]) values (1000, 'Neff Ave.', 'Harrisonburg', 'VA','United States', 22801);
 insert into address ([HouseNumber],[Street],[City],[State],[Country],[Zipcode]) values (800, 'Port Republic rd.', 'Harrisonburg', 'VA','United States', 22801);
@@ -657,19 +628,19 @@ insert into interest(interestname) values ('Electrical Service Repair')
 insert into interest(interestname) values ('Nursing')
 insert into interest(interestname) values ('Dentistry')
 
-insert into person(firstname, lastname, email, addressID, persontype, PhoneNumber) values ('Kristian', 'Andrews', 'kandrews15@gmail.com', 5, 'Student', '703-939-4557')
-insert into person(firstname, lastname, email, addressID, persontype,  PhoneNumber) values ('Ariel', 'Pena', 'apena17@gmail.com', 6, 'Student', '703-939-4557')
-insert into person(firstname, lastname, email, addressID, persontype, PhoneNumber) values ('Alisha', 'Hobbs', 'ahobbs142@gmail.com', 1, 'Student', '703-939-4557')
-insert into person(firstname, lastname, email, addressID, persontype, PhoneNumber) values ('Trevor', 'Singleton', 'Tsingleton@gmail.com', 1, 'Student', '703-939-4557')
-insert into person(firstname, lastname, email, addressID, persontype, PhoneNumber) values ('Mario', 'Fleming', 'Fleming32@gmail.com', 2, 'Student', '703-939-4557')
-insert into person(firstname, lastname, email, addressID, persontype, PhoneNumber) values ('Javier', 'Ramirez', 'cr66@gmail.com', 3, 'Student', '703-939-4557')
-insert into person(firstname, lastname, email, addressID, persontype, PhoneNumber) values ('Trinity', 'Collins', 'collinsT@gmail.com', 4, 'Student', '703-939-4557')
-insert into person(firstname, lastname, email, addressID, persontype, PhoneNumber) values ('Elliot', 'Ball', 'ballE@gmail.com', 5, 'Student', '703-939-4557')
-insert into person(firstname, lastname, email, addressID, persontype, PhoneNumber) values ('Alexis', 'Valencia', 'Valenciaalex@aol.com', 3, 'Student', '703-939-4557')
-insert into person(firstname, lastname, email, addressID, persontype, PhoneNumber) values ('Journey', 'Glover', 'JGlover1022@verizon.net', 5, 'Student' , '703-939-4557')
-insert into person(firstname, lastname, email, addressID, persontype, PhoneNumber) values ('Kevin', 'Johnson', 'kjohnn@gmail.com', 2, 'Student', '703-939-4557')
-insert into person(firstname, lastname, email, addressID, persontype, PhoneNumber) values ('William', 'Evans', 'williamev@gmail.com', 2, 'Student', '703-939-4557')
-insert into person(firstname, lastname, email, addressID, persontype, PhoneNumber) values ('Johnny', 'Pulitano', 'jpulli@gmail.com', 5, 'Student', '703-939-4557')
+insert into person(firstname, lastname, email, addressID, persontype) values ('Kristian', 'Andrews', 'kandrews15@gmail.com', 5, 'Student')
+insert into person(firstname, lastname, email, addressID, persontype) values ('Ariel', 'Pena', 'apena17@gmail.com', 6, 'Student')
+insert into person(firstname, lastname, email, addressID, persontype) values ('Alisha', 'Hobbs', 'ahobbs142@gmail.com', 1, 'Student')
+insert into person(firstname, lastname, email, addressID, persontype) values ('Trevor', 'Singleton', 'Tsingleton@gmail.com', 1, 'Student')
+insert into person(firstname, lastname, email, addressID, persontype) values ('Mario', 'Fleming', 'Fleming32@gmail.com', 2, 'Student')
+insert into person(firstname, lastname, email, addressID, persontype) values ('Javier', 'Ramirez', 'cr66@gmail.com', 3, 'Student')
+insert into person(firstname, lastname, email, addressID, persontype) values ('Trinity', 'Collins', 'collinsT@gmail.com', 4, 'Student')
+insert into person(firstname, lastname, email, addressID, persontype) values ('Elliot', 'Ball', 'ballE@gmail.com', 5, 'Student')
+insert into person(firstname, lastname, email, addressID, persontype) values ('Alexis', 'Valencia', 'Valenciaalex@aol.com', 3, 'Student')
+insert into person(firstname, lastname, email, addressID, persontype) values ('Journey', 'Glover', 'JGlover1022@verizon.net', 5, 'Student')
+insert into person(firstname, lastname, email, addressID, persontype) values ('Kevin', 'Johnson', 'kjohnn@gmail.com', 2, 'Student')
+insert into person(firstname, lastname, email, addressID, persontype) values ('William', 'Evans', 'williamev@gmail.com', 2, 'Student')
+insert into person(firstname, lastname, email, addressID, persontype) values ('Johnny', 'Pulitano', 'jpulli@gmail.com', 5, 'Student')
 
 insert into student(PersonID, DateOfBirth, AcademicYear, SchoolID) values (1, '3/18/2005', 'Freshman', 2)
 insert into student(PersonID, DateOfBirth, AcademicYear, SchoolID) values (2, '2/18/2001', 'Senior', 2)
@@ -746,4 +717,3 @@ insert into StudentInterest(interestID, StudentID) values (9,13)
 insert into person(FirstName, LastName, Email, AddressID, personType) values ('Nick', 'Barth', 'nb@gmail.com', 1, 'Employer')
 insert into Employer(personid, isApproved, EmployerName) values (14, 'yes','James Madison University')
 insert into [dbo].[Opportunity](opportunityname, opportunitytype, isapprenticeship) values('Programming', 'hey', 'yes')
-	       
